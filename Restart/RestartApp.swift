259c9dc6dc18929246e811340f1ace52098ddd21
Pranav Nithesh J <pranavnithesh@gmail.com>
2022-06-03 07:39:13 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by M1089943 on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
