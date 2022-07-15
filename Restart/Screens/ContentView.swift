6bd494ade57728e48c244dc66164ff3014fe8340
Pranav Nithesh J <pranavnithesh@gmail.com>
2022-07-15 16:09:41 +0530

    Restart App

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by M1089943 on 03/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
